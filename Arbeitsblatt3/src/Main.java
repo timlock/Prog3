038686bab0209480580dda7af123da2fb04e369c
timlock <81235388+timlock@users.noreply.github.com>
2021-11-19 22:44:54 +0100

    3.13

--- Arbeitsblatt3/src/Main.java
+++ Arbeitsblatt3/src/Main.java
@@ -1,9 +1,24 @@
-
+import java.util.ArrayList;
 
 public class Main<T> {
     public static void main(String[] args) {
         Ringpuffer<Integer> puffer = new Ringpuffer<>(5);
-        Integer[] integers = {1,2,3,4,5};
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        list1.add(5);
+ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(2);
+        list2.add(3);
+
+
+        puffer.addAll(list1);
+        System.out.println(puffer);
+//        puffer.remove();
+//        System.out.println(puffer);
+        puffer.retainAll(list2);
         System.out.println(puffer);
     }
 }
--- Arbeitsblatt3/src/Ringpuffer.java
+++ Arbeitsblatt3/src/Ringpuffer.java
@@ -18,17 +18,21 @@ public class Ringpuffer<T> implements Queue<T>, Serializable, Cloneable {
     private boolean fixedCapacity = true;
     private boolean discarding = false;
 
-    public Ringpuffer(){
+    public Ringpuffer() {
         elements = new ArrayList<T>();
+        fillUp(capacity);
     }
-    public Ringpuffer(int capacity){
+
+    public Ringpuffer(int capacity) {
         this.capacity = capacity;
+        elements = new ArrayList<T>();
+        fillUp(capacity);
     }
-    public Ringpuffer(int capacity,boolean fixedCapacity, boolean discarding) {
-        this.capacity = capacity;
+
+    public Ringpuffer(int capacity, boolean fixedCapacity, boolean discarding) {
+        this(capacity);
         this.fixedCapacity = fixedCapacity;
         this.discarding = discarding;
-        elements = new ArrayList<T>();
     }
 
     public int readIncrement() {
@@ -40,8 +44,9 @@ public class Ringpuffer<T> implements Queue<T>, Serializable, Cloneable {
         writePos = (writePos + 1) % capacity;
         return writePos;
     }
-    public void fillUp(int requiredSpace){
-        for(int i = 0; i < requiredSpace; i++){
+
+    public void fillUp(int requiredSpace) {
+        for (int i = 0; i < requiredSpace; i++) {
             elements.add(writePos + i, null);
         }
     }
@@ -67,25 +72,33 @@ public class Ringpuffer<T> implements Queue<T>, Serializable, Cloneable {
     @Override
     public Iterator<T> iterator() {
         Iterator<T> iterator = new Iterator<T>() {
-            private int index = readPos;
+            private int index = (readPos - 1) % capacity;
+            private int remainingElements = size;
 
             @Override
             public boolean hasNext() {
-                if (!isEmpty()) return false;
-                if (size < capacity && index < writePos) {
-                    return true;
-                } else if ((index + 1) % capacity != writePos) {
-                    return true;
-                }
-                return false;
+//                if (isEmpty()) return false;
+//                 if ((index + 1) % capacity != writePos) {
+//                    return true;
+//                }
+                if (remainingElements == 0) return false;
+                else return true;
             }
 
             @Override
             public T next() {
+                T rekursiveVar;
                 if (!hasNext()) throw new NoSuchElementException("Es existiert kein weiteres Element");
-                else {
-                    return elements.get(index = (index + 1) % capacity);
+                if(elements.get((index + 1) % capacity) == null) {
+                    index++;
+                    rekursiveVar = next();
                 }
+                else {
+                    index = (index + 1) % capacity;
+                    remainingElements--;
+                    return elements.get(index);
+                }
+                return rekursiveVar;
             }
         };
         return iterator;
@@ -95,40 +108,32 @@ public class Ringpuffer<T> implements Queue<T>, Serializable, Cloneable {
     public Object[] toArray() {
         Object[] objArray = new Object[size];
         for (int i = 0; i < capacity; i++) {
-            objArray[i] = (Object) elements.get((readPos +  i) % capacity);
+            objArray[i] = (Object) elements.get((readPos + i) % capacity);
         }
         return objArray;
     }
 
     @Override
     public <T1> T1[] toArray(T1[] a) {
-        if(a.length < size) a = (T1[]) Array.newInstance(a.getClass(),size);
+        if (a.length < size) a = (T1[]) Array.newInstance(a.getClass(), size);
         for (int i = 0; i < capacity; i++) {
-            a[i] = (T1) elements.get((readPos +  i) % capacity);
+            a[i] = (T1) elements.get((readPos + i) % capacity);
         }
         return null;
     }
 
     @Override
     public boolean add(T t) {
-        Scanner eingabe = new Scanner(System.in);
-        //Puffer darf nicht beschrieben werden
-        if (!discarding) {
-            System.out.println("Puffer ist voll und darf nicht überschrieben werden\n" +
-                    "soll der Puffer wieder Elemente überschreiben dürfen? 1) ja 2) nein");
-            int antwort;
-            do {
-                antwort = eingabe.nextInt();
-            } while (antwort == 1 || antwort == 2);
-            if (antwort == 1) discarding = false;
-            add(t);
+        if(discarding && size == capacity) {
+            System.out.println("Puffer ist voll und Elemente müssen entnommen werden um neue Elemente aufzunehmen");
+            return false;
         }
-        //Puffer ist voll
+        Scanner eingabe = new Scanner(System.in);
         if (size == capacity) {
-            System.out.println("Kapazitätsgrenze wurde erreicht, soll der Puffer\n1) das Element "
-                    + elements.get(readPos) + "überschreiben?\n" +
-                    "2)Keine neuen Elemente mehr aufnehmen?\n" +
-                    "Die Kapazität erhöhen?");
+            System.out.println("Kapazitätsgrenze wurde erreicht, soll der Puffer\n1)Das Element "
+                    + elements.get(readPos) + " überschreiben?\n" +
+                    "2)Die Schreibrechte ändern?\n" +
+                    "3)Die Kapazität erhöhen?");
 
             int antwort;
             do {
@@ -138,31 +143,33 @@ public class Ringpuffer<T> implements Queue<T>, Serializable, Cloneable {
                         readIncrement();
                         writePos = readPos;
                         elements.set(writePos, t);
-                        System.out.println("Element wurde überschrieben");
+                        System.out.println(t + " hat eine Speicherstelle überschrieben");
                         return true;
                     }
 
                     case 2: {
-                        discarding = false;
-                        System.out.println("Puffer kann keine neuen Elemente mehr aufnehmen");
-                        return false;
+                        System.out.println("Puffer kann keine neuen Elemente mehr aufnehmen, soll der Puffer Elemente überschreiben dürfen?\n1) Ja\n2)Nein");
+                        if (eingabe.nextInt() == 1) discarding = false;
+                        else discarding = true;
+                        return add(t);
+
                     }
 
                     case 3: {
                         System.out.println("Bitte die Anzahl neuer zusätzlicher Speicherstellen angeben");
                         int newCapacity = eingabe.nextInt();
                         capacity += newCapacity;
+                        readPos = (readPos + newCapacity) % capacity;
                         fillUp(newCapacity);
-                        add(t);
+                        return add(t);
                     }
-                    break;
                 }
-            } while (antwort < 4 && antwort > 0);
-            return true;
+            } while (antwort > 4 || antwort < 0);
         }
+        System.out.println("Füge " + t + " zur Ringpuffer hinzu");
         elements.set(writePos, t);
         writeIncrement();
-        if (writePos == readPos) readIncrement();
+//        if (writePos == readPos) readIncrement();
         size++;
         return true;
 
@@ -170,7 +177,8 @@ public class Ringpuffer<T> implements Queue<T>, Serializable, Cloneable {
 
     @Override
     public boolean remove(Object o) {
-        if(!elements.contains(o)) return false;
+        if (!elements.contains(o)) return false;
+        System.out.println("Entferne " + o + " aus dem Puffer");
         elements.remove(o);
         size--;
         fillUp(1);
@@ -179,46 +187,52 @@ public class Ringpuffer<T> implements Queue<T>, Serializable, Cloneable {
 
     @Override
     public boolean containsAll(Collection<?> c) {
-       return elements.containsAll(c);
+        return elements.containsAll(c);
     }
 
     @Override
     public boolean addAll(Collection<? extends T> c) {
-        if(c.isEmpty()) return false;
+        if (c.isEmpty()) return false;
         int altered = 0;
-        for (T i:
-             c) {
-            if(add(i)) altered++;
+        for (T i :
+                c) {
+            if (add(i)) altered++;
         }
-        if(altered == 0) return false;
+        if (altered == 0) return false;
         return true;
     }
 
-    public boolean removeElements(CollectionLambda function, Collection<?> c){
-        if(c.isEmpty()) return false;
+    public boolean removeElements(CollectionLambda function, Collection<?> c) {
+        if (c.isEmpty()) return false;
         int oldSize = size;
         function.execute(c);
         fillUp(c.size());
         size = size - c.size();
-        if(oldSize > size) return false;
+        if (oldSize == size) return false;
+        while (elements.get(writePos) == null) writeIncrement();
+        while (elements.get(readPos) == null) readIncrement();
+
         return true;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        CollectionLambda<T> retain = ((a)-> elements.removeAll(a));
-        return removeElements(retain,c);
+        System.out.println("Entferne" + c);
+        CollectionLambda<T> retain = ((a) -> elements.removeAll(a));
+        return removeElements(retain, c);
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        CollectionLambda<T> remove = ((a)-> elements.retainAll(a));
-        return removeElements(remove,c);
+        System.out.println("Entferne alle Elemente außer" + c);
+        CollectionLambda<T> remove = ((a) -> elements.retainAll(a));
+        return removeElements(remove, c);
     }
 
     @Override
     public void clear() {
-        for(int i = 0; i < capacity; i++){
+        System.out.println("Ringpuffer wird geleert");
+        for (int i = 0; i < capacity; i++) {
             elements.set(i, null);
         }
 
@@ -226,18 +240,19 @@ public class Ringpuffer<T> implements Queue<T>, Serializable, Cloneable {
 
     @Override
     public boolean offer(T t) {
-        if(size == capacity) return false;
+        if (size == capacity && fixedCapacity) return false;
         add(t);
         return true;
     }
 
     @Override
-    public T remove() throws NoSuchElementException{
-        if(size == 0) throw new NoSuchElementException("Es existiert kein weiteres Element");
-        if(readPos == writePos) throw new NoSuchElementException("Der Puffer ist leer");
+    public T remove() throws NoSuchElementException {
+        if (size == 0) throw new NoSuchElementException("Der Puffer ist leer");
 //        T output = elements.get(readPos);
+        System.out.println("Entferne " + elements.get(readPos));
         readIncrement();
-        return elements.get((readPos-1) % capacity);
+        size--;
+        return elements.get((readPos - 1) % capacity);
     }
 
     @Override
@@ -247,21 +262,25 @@ public class Ringpuffer<T> implements Queue<T>, Serializable, Cloneable {
 
     @Override
     public T element() {
-        if(size == 0) throw new NoSuchElementException("Es existiert kein weiteres Element");
+        if (size == 0) throw new NoSuchElementException("Es existiert kein weiteres Element");
         return elements.get(readPos);
     }
 
     @Override
     public T peek() {
-        if(size == 0) return null;
+        if (size == 0) return null;
         return elements.get(readPos);
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         Iterator iterator = iterator();
-        while(iterator.hasNext()){
-            System.out.print("");
+        String output = "Inhalt vom Ringpuffer (size:" + size + " Lesepositon: " + readPos + " Schreibposition: " + writePos + ")\n[";
+        while (iterator.hasNext()) {
+            output += iterator.next();
+            if(iterator.hasNext()) output += ", ";
         }
+        output += "]";
+        return output;
     }
 }
